02f86b85253ec284b091b47dd1b644dcff25ad7d
vbuilt20-pixel <pp9361682@gmail.com>
2026-07-18 14:15:45 +0530

    Fix nested routing white screen by switching to BrowserRouter and absolute base path

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App'
@@ -10,9 +10,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )
